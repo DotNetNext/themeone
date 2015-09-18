f4a0e1be87ea70504c0b151a03b486b935e4baea
sunkaixuan <610262374@qq.com>
2015-09-19 01:49:46 +0800

    master_detail

--- ThemeOne/Areas/GridDemo/Controllers/MasterDetailApp/MasterDetailController.cs
+++ ThemeOne/Areas/GridDemo/Controllers/MasterDetailApp/MasterDetailController.cs
@@ -3,17 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JQWidgetsSugar;
+using ThemeOne.Infrastructure;
+using SqlSugar;
+using ThemeOne.Entities;
 
 namespace ThemeOne.Areas.GridDemo.Controllers.MasterDetailApp
 {
     public class MasterDetailController : Controller
     {
- 
 
         public ActionResult Index()
         {
+            ViewBag.Grid = JQXGrid.BindGrid("#grid", MasterDetailGrid.GetDataAdapterSource(), MasterDetailGrid.GetGridConfig());
             return View();
         }
+        public JsonResult GetListSource(GridSearchParams pars)
+        {
 
+            using (ListService ls = new ListService())
+            {
+                if (pars.sortdatafield == null)
+                { //默认按id降序
+                    pars.sortdatafield = "id";
+                    pars.sortorder = "desc";
+                }
+                Sqlable sable = ls.GetListSqlable();
+                var model = JQXGrid.GetWidgetsSource<list>(sable, pars, "*");//根据grid的参数自动查询
+                return Json(model, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult GetListSourceById(GridSearchParams pars,int whereId)
+        {
+
+            using (ListService ls = new ListService())
+            {
+                if (pars.sortdatafield == null)
+                { //默认按id降序
+                    pars.sortdatafield = "id";
+                    pars.sortorder = "desc";
+                }
+                Sqlable sable = ls.GetListSqlable().Where("id=@id");
+                var model = JQXGrid.GetWidgetsSource<list>(sable, pars, "*", new  { id=whereId});//根据grid的参数自动查询
+                return Json(model, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
---
+++ ThemeOne/Areas/GridDemo/Views/_js/master_detail.js
@@ -0,0 +1,74 @@
+﻿var $page = function () {
+
+    this.init = function () {
+        addEvent();
+    }
+
+    //所有元素选择器
+    var selector = {
+        $form: function () { return $("form") },
+        $return_url: function () { return $("#return_url") }
+    }; //selector end
+
+    //所有事件
+    function addEvent() {
+        //绑定验证事件
+
+
+
+    }; //addEvent end
+
+
+    this.grid = {
+        //加载2级表格
+        initRowDetails: function (id, row, element, rowinfo) {
+            element.append($("<div style='margin: 10px;'></div>"));
+            var source =
+            {
+                dataType: "json",
+                dataFields: [{ "name": "id", "type": "int" },
+                            { "name": "name", "type": "string" },
+                            { "name": "productname", "type": "string" },
+                            { "name": "quantity", "type": "int" },
+                            { "name": "date", "type": "date"}],
+                url: "/GridDemo/MasterDetail/GetListSourceById"
+            };
+            var dataAdapter = new $.jqx.dataAdapter(source,
+                {
+                    formatData: function (data) {
+                        $.extend(data, {
+                            whereId: row.id
+                        });
+                        return data;
+                    }
+                }
+            );
+            element.find(">div:first").jqxDataTable(
+            {
+                width: 400,
+                pageable: true,
+                height: 160,
+                pagerButtonsCount: 5,
+                source: dataAdapter,
+                columnsResize: true,
+                columns: [
+                     { text: '编号', dataField: 'id', width: 200 },
+                     { text: '姓名', dataField: 'productname', width: 200 }
+                ]
+            });
+        }
+
+    }
+
+
+    var tool = {
+
+    }; // tool end
+
+};
+
+
+$(function () {
+    var page = new $page();
+    page.init();
+})
