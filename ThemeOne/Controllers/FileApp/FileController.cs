42212522dc43fdeb305505d957a1cbcd615a44ce
sunkaixuan <sunkaixuan>
2015-09-18 17:27:59 +0800

    UploadImg

---
+++ ThemeOne/Controllers/FileApp/FileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SyntacticSugar;
+namespace ThemeOne.Controllers
+{
+    public class FileController : Controller
+    {
+        //
+        // GET: /File/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string UploadImg()
+        {
+            UploadImage ui = new UploadImage();
+            string url = "/views/_upload/temp/{0}.jpg".ToFormat(Guid.NewGuid());
+            string savePath = Server.MapPath(url);
+            HttpPostedFile file = System.Web.HttpContext.Current.Request.Files[0];
+            var reponseMessage = ui.FileSaveAs(file, savePath);
+            return reponseMessage.WebPath;
+        }
+
+    }
+}
